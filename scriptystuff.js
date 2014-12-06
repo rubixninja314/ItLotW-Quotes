acd01d2d63e7ef53cd742bc2ba39aeedd43bc787
rubixninja314 <lyleblowry@gmail.com>
2014-12-06 11:26:25 -0600

    Update scriptystuff.js

--- scriptystuff.js
+++ scriptystuff.js
@@ -1,29 +1,34 @@
-/*function setupQuotes(){
+var doColl=true;
+function setupQuotes(){
 	var cats=["magic","details","evidence","people"];
 	for(var cat=0;cat<4;cat++){
 		var quotes=document.getElementById(cats[cat]).getElementsByClassName("expandable")[0].children;
 		for(var i=0;i<quotes.length;i++){
 			quotes[i].id=cats[cat]+i;
-			quotes[i].onclick="toggleQ("+quotes[i].id+")";
+			quotes[i].addEventListener("click",function{toggleQ(this)},false);
 		}
 	}
-}*/
+}
 function toggleQ(thing){
+	doColl=false
 	thing.innerHTML="hello  world";
 }
 
 
 function toggle(id){
-	var thing = document.getElementById(id);
-	if(thing.className=="generic"){
-		collapse(document.getElementById("magic"));
-		collapse(document.getElementById("details"));
-		collapse(document.getElementById("evidence"));
-		collapse(document.getElementById("people"));
-		expand(thing);
-	}else{
-		collapse(thing);
+	if(doColl){
+		var thing = document.getElementById(id);
+		if(thing.className=="generic"){
+			collapse(document.getElementById("magic"));
+			collapse(document.getElementById("details"));
+			collapse(document.getElementById("evidence"));
+			collapse(document.getElementById("people"));
+			expand(thing);
+		}else{
+			collapse(thing);
+		}
 	}
+	coColl=true;
 }
 function expand(thing){
 	thing.className="expanded";
