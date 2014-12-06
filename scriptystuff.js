b7d135215f2cb103357b7ecb1572acd1249edcf2
rubixninja314 <lyleblowry@gmail.com>
2014-12-06 11:02:30 -0600

    Update scriptystuff.js

--- scriptystuff.js
+++ scriptystuff.js
@@ -3,11 +3,12 @@ function setupQuotes(){
 	for(var cat=0;cat<4;cat++){
 		var quotes=document.getElementById(cats[cat]).getElementsByClassName("expandable")[0].children;
 		for(var i=0;i<quotes.length;i++){
-			quotes[i].addEventListener("click",toggleQ(quotes[i]);
+			quotes[i].id=cats[cat]+i;
+			quotes[i].onclick="toggleQ("+quotes[i].id+")";
 		}
 	}
 }
-function toggleQ(thing){
+function toggleQ(id){
 	thing.innerHTML="hello  world";
 }
 
