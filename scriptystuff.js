252ac15e85094f32fc592b38df19d6ac76c5d22a
rubixninja314 <lyleblowry@gmail.com>
2014-12-06 10:27:59 -0600

    Update scriptystuff.js

--- scriptystuff.js
+++ scriptystuff.js
@@ -1,16 +1,33 @@
+function setupQuotes(){
+	var cats=["magic","details","evidence","people"];
+	for(var cat=0,cat<4,cat++){
+		var quotes=document.getElementById(cats[cat]).getElementsByClassName("expandable")[0].children;
+		for(var i=0,i<quotes.length,i++){
+			quotes[i].addEventListener("click",function(){
+				toggleQ(quotes[i].id);
+			})
+		}
+	}
+}
+toggleQ(id){
+	var thing = document.getElementById(id);
+	thing.innerHTML="helo world";
+}
+
+
 function toggle(id){
 	var thing = document.getElementById(id);
 	if(thing.className="generic"){
+		collapse(document.getElementById("magic"));
+		collapse(document.getElementById("details"));
+		collapse(document.getElementById("evidence"));
+		collapse(document.getElementById("people"));
 		expand(thing);
 	}else{
 		collapse(thing);
 	}
 }
 function expand(thing){
-	collapse(document.getElementById("magic"));
-	collapse(document.getElementById("details"));
-	collapse(document.getElementById("evidence"));
-	collapse(document.getElementById("people"));
 	thing.className="expanded";
 }
 function collapse(thing){
